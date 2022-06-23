62fa7fb6555875b5158870a90e48aef2c93f0564
Manoj <yogeshwaranmanoharan@gmail.com>
2022-06-23 21:25:52 +0530

    - adding increment with payload

--- src/counter.tsx
+++ src/counter.tsx
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { RootState } from "./store/store";
-import { useSelector, useDispatch } from "react-redux";
-import { decrement, increment } from "./store/slices/counterSlice";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+} from "./store/slices/counterSlice";
 
 export function Counter() {
   const count = useSelector((state: RootState) => state.counter.value);
   const dispatch = useDispatch();
 
+  const [incrementAmount, setIncrementByAmount] = useState("0");
+
   return (
     <div>
       <div>
+        <input
+          type="text"
+          value={incrementAmount}
+          onChange={(e) => setIncrementByAmount(e.target.value)}
+        />
+        <button
+          aria-label="Increment by value"
+          onClick={() => {
+            dispatch(incrementByAmount(Number(incrementAmount) || 0));
+          }}
+        >
+          Increment by amount
+        </button>
+        <br />
         <button
           aria-label="Increment value"
           onClick={() => dispatch(increment())}
