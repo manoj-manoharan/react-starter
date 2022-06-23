5499873cf57e0a8db8161e41d3167b2fb683c30a
Manoj <yogeshwaranmanoharan@gmail.com>
2022-06-23 21:13:17 +0530

    - Redux setup with react toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { Counter } from "./counter";
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Counter />
     </div>
   );
 }
